661b6f54b8e4f9d0c69180d70afb87e012b40bbf
ewertonbn <ewertonbn.dev@gmail.com>
2024-05-14 00:18:50 -0300

    feat: change style of task if is checked

--- src/components/tasksList.tsx
+++ src/components/tasksList.tsx
@@ -1,5 +1,6 @@
 import { FormEvent } from 'react'
 import { Trash } from 'phosphor-react'
+import { twMerge } from 'tailwind-merge'
 
 import { ButtonCheckedCustom } from './buttonCheckedCustom'
 
@@ -31,13 +32,24 @@ export function TasksList({
 
   return (
     <div className="mt-6">
-      <div className="mt-3 flex items-start justify-between gap-3 rounded-lg border border-gray-400 bg-gray-500 p-4 first:mt-0">
+      <div
+        className={twMerge(
+          'mt-3 flex items-start justify-between gap-3 rounded-lg border border-gray-400 bg-gray-500 p-4 first:mt-0',
+          data.isChecked ? 'border-gray-500' : 'border-gray-400',
+        )}
+      >
         <ButtonCheckedCustom
           isChecked={data.isChecked}
           id={data.id}
           onClick={handleToggleTask}
         />
-        <p className="flex-1 text-sm">{data.content}</p>
+        {data.isChecked ? (
+          <p className="flex-1 text-sm text-gray-300">
+            <del>{data.content}</del>
+          </p>
+        ) : (
+          <p className="flex-1 text-sm">{data.content}</p>
+        )}
         <button
           type="button"
           className="flex h-6 w-6 items-center justify-center rounded-[4px] text-gray-300 transition-colors hover:bg-gray-400 hover:text-danger"
